92db07411eb7e12f25efe1ae39c3d8464ef22acf
ichethan01 <157832755+ichethan01@users.noreply.github.com>
2024-12-08 12:13:20 -0500

    Delete p6 directory

--- p6/flask_app/config.py
+++
@@ -1,3 +0,0 @@
-# Stores all configuration values
-SECRET_KEY = b'\xf0\xd9(Z\xed\x98B\x19\x9b\x1awm#\xb0\xc1\xe1'
-MONGODB_HOST = 'mongodb+srv://ichethan01:Firebird01@cluster0.okfr7.mongodb.net/my_database?retryWrites=true&w=majority&appName=Cluster0'
