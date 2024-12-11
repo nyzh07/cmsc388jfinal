1efa367541e17d0a4f1b2c98fd0b469a1a12f90a
ichethan01 <157832755+ichethan01@users.noreply.github.com>
2024-12-11 14:02:31 -0500

    Delete flask_app directory

--- flask_app/config.py
+++
@@ -1,3 +0,0 @@
-# Stores all configuration values
-SECRET_KEY =  b'\x9b\xbdo\xc0s\t*\x12\xb3(\x81:\x94,\t\xa6'
-MONGODB_HOST = 'mongodb+srv://althea:1216@p3.zkthc.mongodb.net/results?retryWrites=true&w=majority&appName=p3' 
